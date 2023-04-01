9ed11f393eb2242ab317bdacc1128a31b0e47ef9
JoasChoque <joaschoque@hotmail.com>
2023-03-31 21:16:04 -0300

    Resolvendo desafio Controller

--- src/main/java/com/linuxtips/challengejavaweek/controller/ProdutoController.java
+++ src/main/java/com/linuxtips/challengejavaweek/controller/ProdutoController.java
@@ -2,6 +2,7 @@ package com.linuxtips.challengejavaweek.controller;
 
 
 import com.linuxtips.challengejavaweek.model.Produto;
+import com.linuxtips.challengejavaweek.repository.ProdutoRepository;
 import com.linuxtips.challengejavaweek.service.ProdutoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,5 +14,36 @@ import java.util.List;
 @RequestMapping("/api/")
 public class ProdutoController {
 
-    
+    private ProdutoController(ProdutoService produtoService){
+        this.produtoService = produtoService;
+    };
+
+    private ProdutoService produtoService;
+
+    /*Listar todos os produtos*/
+    @GetMapping("/produtos")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Produto> listarProdutos(){
+        return produtoService.listarProdutos();
+    }
+
+    /*Listar produtos pelo ID*/
+    @GetMapping("/produtos/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Produto> listarPorId(@PathVariable(value = "id") Long id){
+        return produtoService.buscarProdutoPeloId(id);
+    }
+
+    /*Atualizar Produto pelo ID*/
+    @PutMapping("/cursos/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Produto> atualizarPorId(@PathVariable(value="id")Long id,@RequestBody Produto produto){
+        return produtoService.atualizarProdutoPeloId(produto,id);
+    }
+
+    @DeleteMapping("/cursos/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Object> deletarPorId(@PathVariable(value="id") Long id){
+        return produtoService.apagarPeloId(id);
+    }
 }
